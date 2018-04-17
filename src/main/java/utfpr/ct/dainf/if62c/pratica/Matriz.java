acc307f56c7d3d26234b28059e0e544b7689ce89
a1986775 <a1986775@rle-desktop>
2018-04-17 14:04:27 -0300

    Exercicio Pratico 3.3_1

---
+++ src/main/java/utfpr/ct/dainf/if62c/pratica/Matriz.java
@@ -0,0 +1,106 @@
+package utfpr.ct.dainf.if62c.pratica;
+
+/**
+ * Representa uma matriz de valores {@code double}.
+ * @author Wilson Horstmeyer Bogadao <wilson@utfpr.edu.br>
+ */
+public class Matriz {
+    
+    // a matriz representada por esta classe
+    private final double[][] mat;
+    
+    /**
+     * Construtor que aloca a matriz.
+     * @param m O número de linhas da matriz.
+     * @param n O número de colunas da matriz.
+     */
+    public Matriz(int m, int n) {
+        mat = new double[m][n];
+    }
+    
+    /**
+     * Retorna a matriz representada por esta classe.
+     * @return A matriz representada por esta classe
+     */
+    public double[][] getMatriz() {
+        return mat;
+    }
+    
+    /**
+     * Retorna a matriz transposta.
+     * @return A matriz transposta.
+     */
+    public Matriz getTransposta() {
+        Matriz t = new Matriz(mat[0].length, mat.length);
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                t.mat[j][i] = mat[i][j];
+            }
+        }
+        return t;
+    }
+    
+    /**
+     * Retorna a soma desta matriz com a matriz recebida como argumento.
+     * @param m A matriz a ser somada
+     * @return A soma das matrizes
+     */
+    public Matriz soma(Matriz m) {
+        
+        int i,j;
+        Matriz aux = new Matriz(3,2);
+        
+        for(i=0; i<3; i++){
+            for(j=0; j<2; j++){
+                
+                aux.mat[i][j] = m.mat[i][j] + mat[i][j];
+            }
+        }       
+        return aux;
+    }
+
+    /**
+     * Retorna o produto desta matriz com a matriz recebida como argumento.
+     * @param m A matriz a ser multiplicada
+     * @return O produto das matrizes
+     */
+    public Matriz prod(Matriz m) {
+        
+        int i,j,k;
+        Matriz aux = new Matriz(3,2);
+        double v = 0.0;
+        
+        for(i=0; i<mat.length; i++){
+            for(j=0; j<m.mat[0].length; j++){
+                for(k=0; k<mat[0].length; k++){
+                    
+                    v = mat[i][k] * m.mat[k][j] + v;
+                }
+                aux.mat[i][j] = v;
+                v = 0.0;
+            }
+        }        
+        return aux;        
+    }
+
+    /**
+     * Retorna uma representação textual da matriz.
+     * Este método não deve ser usado com matrizes muito grandes
+     * pois não gerencia adequadamente o tamanho do string e
+     * poderia provocar um uso excessivo de recursos.
+     * @return Uma representação textual da matriz.
+     */
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (double[] linha: mat) {
+            s.append("[ ");
+            for (double x: linha) {
+                s.append(x).append(" ");
+            }
+            s.append("]");
+        }
+        return s.toString();
+    }
+    
+}
